e23da5e6a768322631538478281f29a146b88e5c
israel <kevinramirez1j@gmail.com>
2024-11-13 22:51:47 -0600

    corregido

--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,39 +1,39 @@
 <template>
-  <div @click="selectPlan" class="plan">
-    <div class="description">
-      <span class="title">
-        {{ name }} {{ selected ? '🛑' : '' }}
-      </span>
+    <div @click="selectPlan" class="plan">
+      <div class="description">
+        <span class="title">
+          {{ name }} {{ selected ? '🛑' : '' }}
+        </span>
+      </div>
     </div>
-  </div>
-</template>
-
-<script setup>
-// importando ref
-import { ref } from 'vue';
-// Usando un macro para
-// definir las props
-const props = defineProps({
-    name: {
-        type: String,
-        required: true
-    }
-});
-// Define event
-const emit = defineEmits(['select']);
-// Variable que permite
-// recirdar si el plan ha sido seleccionado
-const selected = ref(false);
-// Funcion que permite cammbiar el estado de la variable
-// selected
-const selectPlan = () => {
-  selected.value = true;
-  // Emitir evento de que el
-  // plan ha sido seleccionado
-  emit('select', props.name)
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  // importando ref
+  import { ref } from 'vue';
+  // Usando un macro para
+  // definir las props
+  const props = defineProps({
+      name: {
+          type: String,
+          required: true
+      }
+  });
+  // Define event
+  const emit = defineEmits(['select']);
+  // Variable que permite
+  // recirdar si el plan ha sido seleccionado
+  const selected = ref(false);
+  // Funcion que permite cammbiar el estado de la variable
+  // selected
+  const selectPlan = () => {
+    selected.value = true;
+    // Emitir evento de que el
+    // plan ha sido seleccionado
+    emit('select', props.name)
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
