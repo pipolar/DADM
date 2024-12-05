fa96ea2d455e02a2a9ca5d0912c705d8ce134019
israel <kevinramirez1j@gmail.com>
2024-12-05 12:02:37 -0600

    Introduccion-slots

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,14 @@
 <script setup>
-import PlanPicker from './components/PlanPicker.vue';
-import { ref } from 'vue';
-
-const showPlans = ref(false);
+import fancyButton from './components/FancyButton.vue';
 </script>
 
 <template>
-  <header>
-    <div>
-      <span id="logo">Liquid Gold Box</span>
-      <img src="./assets/logo.png" alt="logo">
-    </div>
-  </header>
-
   <div class="content">
-    <h1 class="title">Coffee Plans</h1>
-
-    <h2 class="subtitle">
-      Viajamos por el mundo para encontrar el mejor café de origen único para ti
-    </h2>
-
-    <label>
-      <input type="checkbox" v-model="showPlans">
-      Mostrar selector de planes
-    </label>
-
-    <!-- TODO INCLUDE PLAN-PICKER COMPONENT -->
-    <PlanPicker v-if="showPlans" />
-
+    <fancyButton>
+    </fancyButton>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/FancyButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <button>Hazme Clic!</button>
+</template>
+  
+<script setup>
+</script>
+  
+<style scoped>
+html {
+font-family: sans-serif;
+}
+</style>
